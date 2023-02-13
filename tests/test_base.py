522b218c67f150f714adcbf4a93fb367c5a2eee1
Jesper Nielsen <jespernielsen1982@gmail.com>
2023-02-13 19:02:27 +0100

    Initial implementation of DictDir.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from typing import Sequence, Type
 
-from typedpath.base import BytesFile, TextFile
+import pytest
+
+from typedpath.base import _K, BytesFile, DictDir, TextFile
 
 
 def test_text_file(tmp_path: Path) -> None:
@@ -27,3 +30,69 @@ def test_bytes_file(tmp_path: Path) -> None:
     assert d.exists()
     assert b"foo" == p.read_bytes()
     assert b"foo" == f.read()
+
+
+@pytest.mark.parametrize(
+    "key_type, keys",
+    [
+        (int, range(3)),
+        (str, "abc"),
+        # (bool, [True, False]),  # TODO
+        (float, [0.1, 6.28, 4.0]),
+    ],
+)
+def test_dict_dir__keys(key_type: Type[_K], keys: Sequence[_K], tmp_path: Path) -> None:
+    d = DictDir(tmp_path, key_type, TextFile)
+    assert not d
+    assert len(d) == 0
+    assert not d.keys()
+    assert not d.values()
+    assert not d.items()
+    for k in keys:
+        assert k not in d
+
+    for k in keys:
+        d[k].write(str(k))
+
+    assert d
+    assert len(d) == len(keys)
+
+    assert set(d.keys()) == set(keys)
+    assert len(d.values()) == len(keys)
+    for v in d.values():
+        assert key_type(v.read()) in keys  # type: ignore[call-arg]
+    assert len(d.items()) == len(keys)
+    for k, v in d.items():
+        assert k == key_type(v.read())  # type: ignore[call-arg]
+    for k in keys:
+        assert k in d
+
+
+def test_dict_dir__bytes_file(tmp_path: Path) -> None:
+    data = {
+        0.1: b"1",
+        6.28: b"2",
+        31337.0: b"3",
+    }
+    d = DictDir(tmp_path, float, BytesFile)
+    assert not d
+    assert len(d) == 0
+    assert not d.keys()
+    assert not d.values()
+    assert not d.items()
+    for k in data:
+        assert k not in d
+
+    for k, v in data.items():
+        d[k].write(v)
+
+    assert d
+    assert len(d) == len(data)
+
+    assert set(d.keys()) == set(data)
+    assert len(d.values()) == len(data)
+    assert len(d.items()) == len(data)
+    for k, v_ in d.items():
+        assert data[k] == v_.read()
+    for k in data:
+        assert k in d
--- typedpath/base.py
+++ typedpath/base.py
@@ -1,24 +1,23 @@
+from abc import ABC
 from os import PathLike
 from pathlib import Path
-from typing import TypeAlias, TypeVar
+from typing import Generic, Iterator, Mapping, Type, TypeAlias, TypeVar
 
-K = TypeVar("K")
-PV = TypeVar("PV", bound="TypedPath")
+_K = TypeVar("_K")
+_PV = TypeVar("_PV", bound="TypedPath")
 
 
 PathLikeLike: TypeAlias = PathLike[str] | str
 
 
-class TypedPath:
+class TypedPath(ABC):
+    default_suffix: str
+
     def __init__(self, path: PathLikeLike) -> None:
         self._path = Path(path)
 
     def __str__(self) -> str:
         return str(self._path)
-
-
-class TypedDir(TypedPath):
-    pass
 
 
 class TypedFile(TypedPath):
@@ -35,6 +34,8 @@ class TypedFile(TypedPath):
 
 
 class TextFile(TypedFile):
+    default_suffix = ".txt"
+
     def write(
         self,
         data: str,
@@ -49,8 +50,55 @@ class TextFile(TypedFile):
 
 
 class BytesFile(TypedFile):
+    default_suffix = ".bin"
+
     def write(self, data: bytes) -> int:
         return self.write_path().write_bytes(data)
 
     def read(self) -> bytes:
         return self.read_path().read_bytes()
+
+
+class TypedDir(TypedPath):
+    def pretty_path(self) -> Path:
+        return self._path
+
+
+class DictDir(TypedDir, Mapping[_K, _PV], Generic[_K, _PV]):
+    default_suffix = ""
+
+    def __init__(self, path: PathLikeLike, key_type: Type[_K], value_type: Type[_PV]) -> None:
+        super().__init__(path)
+
+        self._key_type = key_type
+        self._value_type = value_type
+
+    def _key_to_path(self, key: object) -> Path:
+        key_str = str(key)
+        key_ = self._key_type(key_str)  # type: ignore[call-arg]
+        assert key_ == key, (
+            "DictDir keys does not handle round-trip:"
+            f" {self._key_type.__name__}(str({key}))!={key_}."
+        )
+        assert "/" not in key_str, f"DictDir keys cannot contain '/'. Key: {key_str}"
+        key_name = f"{key_str}{self._value_type.default_suffix}"
+        return self._path / key_name
+
+    def _path_to_key(self, path: Path) -> _K:
+        key_name = path.name
+        key_str = key_name[: -len(self._value_type.default_suffix)]
+        return self._key_type(key_str)  # type: ignore[call-arg]
+
+    def __getitem__(self, key: _K) -> _PV:
+        item_path = self._key_to_path(key)
+        return self._value_type(item_path)
+
+    def __iter__(self) -> Iterator[_K]:
+        for item_path in self._path.iterdir():
+            yield self._path_to_key(item_path)
+
+    def __contains__(self, key: object) -> bool:
+        return self._key_to_path(key).exists()
+
+    def __len__(self) -> int:
+        return len(list(self._path.iterdir()))
