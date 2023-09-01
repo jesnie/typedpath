0a1815df60b6a33eb099429a686e000b66fd3b52
Jesper Nielsen <44195043+jesnie@users.noreply.github.com>
2023-09-01 15:53:01 +0200

    Use compreq to manage requirements. (#10)

---
+++ compreq.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -e
+
+git fetch origin main
+git checkout main
+git pull --rebase origin main
+if ! git checkout -b typedpath; then
+    git branch -D typedpath
+    git checkout -b typedpath
+fi
+poetry run python -m requirements
+if [[ $(git status --porcelain) ]]; then
+    poetry update
+    git \
+        -c "user.name=Update requirements bot" \
+        -c "user.email=none" \
+        commit \
+        -am "Update requirements."
+    git push origin +typedpath
+    gh pr create \
+       --title "Update requirements" \
+       --body "Automatic update of requirements."
+       --reviewer jesnie
+    false
+fi
